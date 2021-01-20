82da29fef79aa984eec2551348cefb0ae5a45b8b
tsq <tangshiqiangcn@gmail.com>
2021-01-20 23:11:14 +0900

    Init

---
+++ README.md
@@ -0,0 +1,4 @@
+# github-webhook-sha256
+
+A expressjs middleware for handling Github webhook using X-Hub-Signature-256 header
+
---
+++ app.js
@@ -0,0 +1,16 @@
+const crypto = require("crypto");
+const sigHeaderName = "x-hub-signature-256";
+
+module.export = (secret) => (req, res, next) => {
+    const signature = req.headers[sigHeaderName];
+    const payload = req.body;
+    const hash = crypto.createHmac("sha256", secret);
+    hash.update(JSON.stringify(payload));
+    const code = hash.digest("hex");
+    const result = "sha256=" + code;
+    if (signature === result) {
+        next();
+    } else {
+        next("signature is wrong");
+    }
+}
